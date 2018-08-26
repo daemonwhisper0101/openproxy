7cc9870648d32a94fa74c578906422108fce3405
daemonwhisper0101 <daemonwhisper0101@users.noreply.github.com>
2018-08-26 10:57:19 +0900

    proxydb: implement disposable DialContext

--- proxydb/check.go
+++ proxydb/check.go
@@ -2,6 +2,7 @@
 package proxydb
 
 import (
+  "context"
   "net"
   "net/http"
   "time"
@@ -9,11 +10,29 @@ import (
   "github.com/daemonwhisper0101/openproxy"
 )
 
-func checkOpenProxy(p openproxy.OpenProxy, url string) uint64 {
+type disposableConn struct {
+  conn net.Conn
+  closed bool
+}
+
+func disposableDialContext(ctx context.Context, network, addr string) (net.Conn, error) {
   d := &net.Dialer{ Timeout: time.Second * 10, KeepAlive: time.Second }
+  conn, err := d.DialContext(ctx, network, addr)
+  if err != nil {
+    return nil, err
+  }
+  // 30 secs alive
+  go func() {
+    time.Sleep(time.Second * 30)
+    conn.Close()
+  }()
+  return conn, nil
+}
+
+func checkOpenProxy(p openproxy.OpenProxy, url string) uint64 {
   tr := &http.Transport{
     Proxy: http.ProxyURL(p.URL()),
-    DialContext: d.DialContext,
+    DialContext: disposableDialContext,
     TLSHandshakeTimeout: time.Second * 5,
     DisableKeepAlives: true,
     IdleConnTimeout: time.Second,
